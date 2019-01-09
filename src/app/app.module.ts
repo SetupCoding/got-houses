24c43e51e20d74631100f6db80712f1e13e7bca2
Anton <>
2019-01-09 20:05:38 +0100

    Added version interceptor, that attaches an Accept Header with the version to each http request.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,13 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
 import {AppComponent} from './app.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {AppRoutingModule} from './app-routing.module';
+import {environment} from '../environments/environment';
 import {iceAndFireApiUrl} from './core/http/ice-and-fire.service';
 import {iceAndFireApiVersion, VersionInterceptor} from './core/interceptors/version.interceptor';
+import {HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -15,10 +17,14 @@ import {iceAndFireApiVersion, VersionInterceptor} from './core/interceptors/vers
     AppRoutingModule,
     BrowserModule,
     BrowserAnimationsModule,
+    HttpClientModule
+  ],
+  providers: [
     {provide: iceAndFireApiUrl, useValue: environment.iceAndFireApi.url},
     {provide: iceAndFireApiVersion, useValue: environment.iceAndFireApi.version},
+    {provide: HTTP_INTERCEPTORS, useClass: VersionInterceptor, multi: true},
   ],
-  providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
