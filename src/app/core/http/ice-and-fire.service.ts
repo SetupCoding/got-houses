1d3ec2f1c640a9f0aca72585963205e86a1ff389
Anton <>
2019-01-10 02:28:14 +0100

    Added first mock implementation of filters in the iceAndFire API Service.

--- src/app/core/http/ice-and-fire.service.ts
+++ src/app/core/http/ice-and-fire.service.ts
@@ -1,6 +1,7 @@
 import {Inject, Injectable, InjectionToken} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {House} from '../../model/house';
+import {HouseFilters} from '../../model/house-filters';
 
 export const iceAndFireApiUrl = new InjectionToken<string>('iceAndFireApiUrl');
 
@@ -14,11 +15,16 @@ export class IceAndFireService {
     this.baseUrl = baseUrl ? baseUrl : '';
   }
 
-  fetchHouses(page = 1, pageSize = 10) {
+  fetchHouses(page = 1, pageSize = 10, filters?: HouseFilters) {
     const requestUrl = new URL(this.baseUrl + '/houses');
     requestUrl.searchParams.set('page', String(page));
     requestUrl.searchParams.append('pageSize', String(pageSize));
-    return this.http.get<House[]>(requestUrl.toString());
+    if (filters) {
+      for (let filterName of Object.keys(filters)) {
+        requestUrl.searchParams.append(filterName, filters[filterName]);
+      }
+    }
+    return this.http.get<House[]>(requestUrl.toString(), {observe: 'response'});
   }
 
   fetchHouse(index: number) {
