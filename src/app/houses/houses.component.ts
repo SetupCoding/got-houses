670cf89e8df19f4fc25a909912ba795e21f6bdcb
Anton <>
2019-01-10 17:43:26 +0100

    Added basic style layout
    Added mat-grid-list to display two tiles (table/detail view) next to each other on big screens.
    Added a breakpointObserver to detect window size changes.
    Added mat-card to the detail view.

--- src/app/houses/houses.component.ts
+++ src/app/houses/houses.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {AfterViewInit, Component, OnInit} from '@angular/core';
+import {IceAndFireService} from '../core/http/ice-and-fire.service';
+import {BreakpointObserver} from '@angular/cdk/layout';
 
 @Component({
   selector: 'app-houses',
@@ -6,10 +8,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./houses.component.css']
 })
 export class HousesComponent implements OnInit {
+  maxColumns: number;
 
-  constructor() { }
+  constructor(private iceAndFireService: IceAndFireService, private breakpointObserver: BreakpointObserver) {
+  }
 
   ngOnInit() {
+    this.iceAndFireService.initializeHouseData();
+    this.subscribeToChanges();
+  }
+
+  subscribeToChanges(): void {
+    const windowSizeChanges = this.breakpointObserver.observe([
+      '(max-width: 768px)'
+    ]);
+
+    windowSizeChanges.subscribe(result => {
+      this.updateMaxColumns(result.matches);
+    });
+  }
+
+  updateMaxColumns(isSmallScreen: boolean): void {
+    this.maxColumns = isSmallScreen ? 1 : 2;
   }
 
 }
