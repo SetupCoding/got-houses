715b12d3bf6b5426c92c9e5cb149d585fc48e27d
Anton <>
2019-01-10 02:02:12 +0100

    Added HouseFilters interface to pre-filter the query.

--- src/app/core/interceptors/version.interceptor.ts
+++ src/app/core/interceptors/version.interceptor.ts
@@ -1,4 +1,4 @@
-import {HttpEvent, HttpHandler, HttpInterceptor, HttpRequest} from '@angular/common/http';
+import {HttpEvent, HttpHandler, HttpInterceptor, HttpRequest, HttpResponse} from '@angular/common/http';
 import {Inject, Injectable, InjectionToken} from '@angular/core';
 import {Observable, throwError} from 'rxjs';
 import {catchError, tap} from 'rxjs/operators';
@@ -8,6 +8,7 @@ export const iceAndFireApiVersion = new InjectionToken<string>('iceAndFireApiVer
 @Injectable({providedIn: 'root'})
 export class VersionInterceptor implements HttpInterceptor {
   version: string;
+  // eTag = '';
 
   constructor(@Inject(iceAndFireApiVersion) version?: string) {
     this.version = version ? version : '';
@@ -15,12 +16,24 @@ export class VersionInterceptor implements HttpInterceptor {
 
 
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    const clonedRequest = req.clone({setHeaders: {Accept: 'application/vnd.anapioficeandfire+json; version=' + this.version}});
-    return next.handle(clonedRequest).pipe(tap(event => {
+    const clonedRequest = req.clone({
+      setHeaders: {
+        Accept: 'application/vnd.anapioficeandfire+json; version=' + this.version
+        // 'If-None-Match': this.eTag,
+        // 'If-Modified-Since': new Date().toISOString()
+      }
+    });
+    return next.handle(clonedRequest).pipe(tap((event: HttpResponse<any>) => {
+      // if (event.type === 4) {
+        // this.handleNewETag(event.headers.get('etag'));
+      // }
     }), catchError((error) => {
       console.error(error);
       return throwError(error);
     }) as any);
+  }
 
+  handleNewETag(eTag: string) {
+    // this.eTag = eTag;
   }
 }
---
+++ src/app/model/house-filters.ts
@@ -0,0 +1,10 @@
+export interface HouseFilters {
+  name: string;
+  region: string;
+  words: string;
+  hasWords: boolean;
+  hasTitles: boolean;
+  hasSeats: boolean;
+  hasDiedOut: boolean;
+  hasAncestralWeapons: boolean;
+}
