40e1525ac747f48cf63d99f3dfd18b3050ee1743
Anton <>
2019-01-14 00:22:50 +0100

    Added extract service test.

--- src/app/core/extract/extract.service.spec.ts
+++ src/app/core/extract/extract.service.spec.ts
@@ -1,6 +1,6 @@
-import { TestBed } from '@angular/core/testing';
+import {TestBed} from '@angular/core/testing';
 
-import { ExtractService } from './extract.service';
+import {ExtractService} from './extract.service';
 
 describe('ExtractService', () => {
   beforeEach(() => TestBed.configureTestingModule({}));
@@ -8,5 +8,9 @@ describe('ExtractService', () => {
   it('should be created', () => {
     const service: ExtractService = TestBed.get(ExtractService);
     expect(service).toBeTruthy();
+  });
+  it('should extract 123 from https://anapioficeandfire.com/api/houses/123', () => {
+    const service: ExtractService = TestBed.get(ExtractService);
+    expect(service.extractIndexFromUrl('https://anapioficeandfire.com/api/houses/229')).toBeTruthy();
   });
 });
