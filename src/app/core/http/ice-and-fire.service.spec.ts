dbc50f31774f987c53204da7af71d078c3152560
Anton <>
2019-01-14 03:04:11 +0100

    Added snack bar service unit test coverage.

--- src/app/core/http/ice-and-fire.service.spec.ts
+++ src/app/core/http/ice-and-fire.service.spec.ts
@@ -1,7 +1,7 @@
 import {HttpClientTestingModule, HttpTestingController} from '@angular/common/http/testing';
 import {async, TestBed} from '@angular/core/testing';
 import {IceAndFireService} from './ice-and-fire.service';
-import {HttpClient, HttpClientModule, HttpErrorResponse} from '@angular/common/http';
+import {HttpClient, HttpClientModule} from '@angular/common/http';
 
 describe('IceAndFireService', () => {
   let service: IceAndFireService;
