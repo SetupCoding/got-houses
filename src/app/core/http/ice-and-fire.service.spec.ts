38ced0b3e1a2bfe2067e7ffcaa58864f3a0b26f0
Anton <>
2019-01-14 02:12:48 +0100

    Added tests to ice and fire service.

--- src/app/core/http/ice-and-fire.service.spec.ts
+++ src/app/core/http/ice-and-fire.service.spec.ts
@@ -1,13 +1,27 @@
-import {TestBed} from '@angular/core/testing';
-
+import {HttpClientTestingModule, HttpTestingController} from '@angular/common/http/testing';
+import {async, TestBed} from '@angular/core/testing';
 import {IceAndFireService} from './ice-and-fire.service';
-import {HttpClientModule} from '@angular/common/http';
+import {HttpClient, HttpClientModule, HttpErrorResponse} from '@angular/common/http';
 
 describe('IceAndFireService', () => {
-  beforeEach(() => TestBed.configureTestingModule({imports: [HttpClientModule]}));
-
+  let service: IceAndFireService;
+  let httpClient: HttpClient;
+  let httpTestingController: HttpTestingController;
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [HttpClientModule, HttpClientTestingModule]
+    });
+    service = TestBed.get(IceAndFireService);
+    httpClient = TestBed.get(HttpClient);
+    httpTestingController = TestBed.get(HttpTestingController);
+  });
   it('should be created', () => {
-    const service: IceAndFireService = TestBed.get(IceAndFireService);
+    service = TestBed.get(IceAndFireService);
     expect(service).toBeTruthy();
   });
+  it('should handle error', async(() => {
+    service = TestBed.get(IceAndFireService);
+    service.fetchHouses();
+    expect(console.error).toBeTruthy();
+  }));
 });
