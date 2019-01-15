e89306bc16f8240d09ea6c45cc03a703b6952e9a
Anton <>
2019-01-15 19:30:15 +0100

    Added e2e tests for filters and pagination.

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -1,6 +1,5 @@
 import {AppPage} from './app.po';
 import {browser} from 'protractor';
-import {timeout} from 'rxjs/operators';
 
 describe('workspace-project App', () => {
   let page: AppPage;
@@ -17,4 +16,79 @@ describe('workspace-project App', () => {
       });
     });
   });
+  it('should select and display 5 items per page', () => {
+    page.getPaginatorSelection().click();
+    page.getFisrtPaginatorSelection().click();
+    expect(page.getrowByNumber(5)).toBeTruthy();
+    expect(page.getrowByNumber(6).isPresent()).toEqual(false);
+  });
+
+  it('should go to page 2', () => {
+    page.getPaginatorSelection().click();
+    page.getPaginatorSelectionByNumber(3).click();
+    page.getNextPage().click();
+    page.getFirstRowFirstCol().getText().then((text: string) => {
+      expect(text === '26').toBeTruthy();
+    });
+  });
+  it('should go back from page 2', () => {
+    page.getPreviousPage().click();
+    page.getFirstRowFirstCol().getText().then((text: string) => {
+      expect(text === '1').toBeTruthy();
+    });
+  });
+  it('should go to first page from page 3', () => {
+    page.getNextPage().click();
+    page.getFirstRowFirstCol().getText().then((text: string) => {
+      expect(text === '26').toBeTruthy();
+    });
+    page.getNextPage().click();
+    page.getFirstRowFirstCol().getText().then((text: string) => {
+      expect(text === '51').toBeTruthy();
+    });
+    page.getFirstPage().click();
+    page.getFirstRowFirstCol().getText().then((text: string) => {
+      expect(text === '1').toBeTruthy();
+    });
+  });
+  it('should go to last page from page 1', () => {
+    page.getLastPage().click();
+    page.getFirstRowFirstCol().getText().then((text: string) => {
+      expect(text === '426').toBeTruthy();
+    });
+  });
+  it('should expand filter header', () => {
+    page.getFilterHeader().click();
+    expect(page.getFilterSelection().isPresent()).toEqual(true);
+  });
+  it('should open filter selection', () => {
+    page.getFilterSelection().click();
+  });
+  it('should addFilter hasWords:true', () => {
+    page.getFilterOptionByNumber(4).click();
+    page.getFilterSelectionCheckbox().click();
+    page.getAddFilterButton().click();
+  });
+  it('should addFilter hasTitles:true', () => {
+    page.getFilterSelection().click();
+    page.getFilterOptionByNumber(5).click();
+    page.getAddFilterButton().click();
+  });
+  it('should removeFilter hasTitles:true', () => {
+    page.getRemoveFilterButtonOfFilterNumber(2).click();
+    expect(page.getFilterChipByNumber(2).isPresent()).toEqual(false);
+  });
+  it('should apply filter hasWords:true', () => {
+    page.getApplyFilterButton().click();
+    page.getFirstRowFirstCol().getText().then((text: string) => {
+      expect(text === '2').toBeTruthy();
+    });
+  });
+  it('should remove all filters', () => {
+    page.getRemoveAllFiltersButton().click();
+    page.getFirstRowFirstCol().getText().then((text: string) => {
+      expect(text === '1').toBeTruthy();
+    });
+  });
 });
+
--- e2e/src/app.po.ts
+++ e2e/src/app.po.ts
@@ -5,15 +5,77 @@ export class AppPage {
     return browser.get('/');
   }
 
-  getTitleText() {
-    return element(by.css('app-root h1')).getText();
-  }
-
   getPaginatorSelection() {
-    return element('#tablePaginator');
+    return element(by.css('mat-select[aria-label="Items per page:"]>div:nth-of-type(1)>div:nth-of-type(1)'));
   }
 
-  getPaginatorSelectionOptions() {
-    return this.getPaginatorSelection().findElement('option');
+
+  getFisrtPaginatorSelection() {
+    return element(by.css('mat-option:nth-of-type(1)>span'));
+  }
+
+  getPaginatorSelectionByNumber(optionNumber: number) {
+    return element(by.css('mat-option:nth-of-type(' + optionNumber + ')>span'));
+  }
+
+  getFilterHeader() {
+    return element(by.css('mat-panel-description'));
+  }
+
+  getFilterSelection() {
+    return element(by.css('app-house-filter>mat-list>mat-list-item>div:nth-of-type(1)>' +
+      'mat-form-field:nth-of-type(1)>div:nth-of-type(1)>div:nth-of-type(1)>div:nth-of-type(1)'));
+  }
+
+  getFilterSelectionCheckbox() {
+    return element(by.css('mat-checkbox>label>div:nth-of-type(1)'));
+  }
+
+  getAddFilterButton() {
+    return element(by.css('mat-list-item:nth-of-type(1)>div:nth-of-type(1)>button>span>mat-icon'));
+  }
+
+  getRemoveFilterButtonOfFilterNumber(filterNumber: number) {
+    return element(by.css('app-house-filter-item:nth-of-type(' + filterNumber + ')>mat-chip>mat-icon'));
+  }
+
+  getApplyFilterButton() {
+    return element(by.css('mat-action-row>button:nth-of-type(2)>span'));
+  }
+
+  getRemoveAllFiltersButton() {
+    return element(by.css('mat-action-row>button:nth-of-type(1)>span>mat-icon'));
+  }
+
+  getFilterChipByNumber(chipNumber: number) {
+    return element(by.css('app-house-filter-item:nth-of-type(' + chipNumber + ')>mat-chip'));
+  }
+
+  getFilterOptionByNumber(optionNumber: number) {
+    return element(by.css('mat-option:nth-of-type(' + optionNumber + ')>span'));
+  }
+
+  getrowByNumber(rowNumber: number) {
+    return element(by.css('mat-row:nth-of-type(' + rowNumber + ')>mat-cell:nth-of-type(3)'));
+  }
+
+  getNextPage() {
+    return element(by.css('button[aria-label="Next page"]'));
+  }
+
+  getLastPage() {
+    return element(by.css('button[aria-label="Last page"]'));
+  }
+
+  getFirstPage() {
+    return element(by.css('button[aria-label="First page"]'));
+  }
+
+  getPreviousPage() {
+    return element(by.css('button[aria-label="Previous page"]'));
+  }
+
+  getFirstRowFirstCol() {
+    return element(by.css('mat-row:nth-of-type(1)>mat-cell:nth-of-type(1)'));
   }
 }
