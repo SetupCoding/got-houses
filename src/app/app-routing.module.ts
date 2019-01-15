11fd99aa12bd6e9ba353288d359e1bf0ef8b505f
Anton <>
2019-01-15 21:06:58 +0100

    Adjusted rerouting to preserve parameters.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,10 @@
 import {NgModule} from '@angular/core';
 import {PreloadAllModules, RouterModule, Routes} from '@angular/router';
-import {AppComponent} from './app.component';
 
 
 const appRoutes: Routes = [
-  {path: '', component: AppComponent},
   {path: 'houses', loadChildren: './houses/houses.module#HousesModule'},
   {path: '**', redirectTo: '/houses'},
-
 
 ];
 
