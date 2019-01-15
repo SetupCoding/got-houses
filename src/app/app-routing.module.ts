2d79ef3b6652e31056f277543f82f639b340137d
Anton <>
2019-01-15 01:24:49 +0100

    Added base route.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import {NgModule} from '@angular/core';
 import {PreloadAllModules, RouterModule, Routes} from '@angular/router';
+import {AppComponent} from './app.component';
 
 
 const appRoutes: Routes = [
-  // {path: '', component: HomeComponent},
+  {path: '', component: AppComponent},
   {path: 'houses', loadChildren: './houses/houses.module#HousesModule'},
-  {path: '**', redirectTo: '/houses'}
+  {path: '**', redirectTo: '/houses'},
+
 
 ];
 
